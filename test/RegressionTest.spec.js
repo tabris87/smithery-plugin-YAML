40764bf4d9bdb6988f8dc8858c4b6c6e52474329
Adrian Marten <Adrian.Marten@msg.group>
2021-09-23 20:16:14 +0200

    Start

---
+++ test/RegressionTest.spec.js
@@ -0,0 +1,7 @@
+const { TestListConcat } = require('./listConcat.test.js');
+//const { TestPropertyCompose } = require('./propertyCompose.test.js');
+
+describe('Regression Testing Rules', () => {
+    TestListConcat();
+    //TestPropertyCompose();
+});
